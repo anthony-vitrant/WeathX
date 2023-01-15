4f3dd2e590d1605c50c32ee92a0f5a4a0f657565
anthony-vitrant <anthony.vitrant@alumni.univ-avignon.fr>
2023-01-15 18:08:37 +0100

    wrote parser for HotelParser

--- src/main/java/Model/HotelParser.java
+++ src/main/java/Model/HotelParser.java
@@ -2,19 +2,42 @@ package Model;
 
 import java.io.IOException;
 import java.util.List;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class HotelParser implements Parser {
     String HotelJson;
     String WeatherMessage;
-    String Message;
+    String HotelMessage;
     @Override
     public String parseJson(List<String> latlon) throws IOException, InterruptedException {
+
         WeatherMessage = new WeatherParser().parseJson(latlon);
         HotelJson = new OpenStreetMapAPI().getHotelsByLatLon(latlon.get(0), latlon.get(1));
-        //TODO
-        //parse the json to get the display name
-        // and return the WeatherMessage + the Message with the display name (5 max)
-        // like return WeatherMessage +"\n"+ Message;
-        return null;
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(HotelJson);
+
+        int counter = 0;
+        for (JsonNode node : jsonNode) {
+            String displayName = node.get("display_name").asText();
+
+            int commaIndex = displayName.indexOf(",");
+            String shortenedString;
+            if (commaIndex != -1) {
+                shortenedString = displayName.substring(0, commaIndex);
+            } else {
+                shortenedString = displayName;
+            }
+            HotelMessage += shortenedString + "\n";
+            System.out.println(shortenedString);
+            counter++;
+            if (counter == 5) { //5times max
+                break;
+            }
+        }
+
+        System.out.println(HotelMessage);
+        return WeatherMessage + "\n" + HotelMessage;
     }
 }
